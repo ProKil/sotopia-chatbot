f42364e769015e1dd6a838d63206671fc0844e7f
Hao Zhu <prokilchu@gmail.com>
2023-08-31 16:18:44 -0400

    ✨ add a dummy episode demo

---
+++ app/render/[id]/page.tsx
@@ -0,0 +1,83 @@
+import { type Metadata } from 'next'
+import { notFound, redirect } from 'next/navigation'
+
+import { auth } from '@/auth'
+import { getChat } from '@/app/actions'
+import { Message } from 'ai'
+
+export const runtime = 'edge'
+export const preferredRegion = 'home'
+
+export interface ChatPageProps {
+  params: {
+    id: string
+  }
+}
+
+
+// environment: str = Field(index=True)
+// agents: list[str] = Field(index=True)
+// tag: str | None = Field(index=True)
+// models: list[str] | None = Field(index=True)
+// messages: list[list[tuple[str, str, str]]]  # Messages arranged by turn
+// reasoning: str
+// rewards: list[
+//     tuple[float, dict[str, float]] | float
+// ]  # Rewards arranged by turn
+// rewards_prompt: str
+
+function chooseOnlyMessagesToEnvironment(messages: any) {
+    return messages.map(
+        messages_in_turn => messages_in_turn.filter((message: any) => message[1] === "Environment")
+    );
+}
+
+async function getEpisode(episodeId: string) {
+    if (process.env.SOTOPIA_SERVER_URL === undefined) {
+        throw new Error("SOTOPIA_SERVER_URL is undefined")
+    } else {
+        console.log(process.env.SOTOPIA_SERVER_URL + "get_episode/" + episodeId)
+        const request = new Request(
+            process.env.SOTOPIA_SERVER_URL + "get_episode/" + episodeId, {
+            method: 'GET',
+        })
+        const response_json = await fetch(request)
+            .then(response  => {
+                if (response.status === 200) {
+                    return response.json();
+                } else {
+                    throw new Error("Something went wrong on API server!");
+                }
+            });
+        return <div>{JSON.stringify(
+            chooseOnlyMessagesToEnvironment(
+                response_json["messages"]
+            )
+        )}</div>;
+    }
+}
+
+export async function generateMetadata({
+  params
+}: ChatPageProps): Promise<Metadata> {
+  const session = await auth()
+
+  if (!session?.user) {
+    return {}
+  }
+
+  const chat = await getChat(params.id, session.user.id)
+  return {
+    title: chat?.title.toString().slice(0, 50) ?? 'Chat'
+  }
+}
+
+export default async function ChatPage({ params }: ChatPageProps) {
+  
+//   if (chat?.userId !== session?.user?.id) {
+//     notFound()
+//   }
+
+//   return <Chat id={chat.id} initialMessages={chat.messages} />
+    return <div>{await getEpisode(params.id)}</div>
+}
