dabe07a875a559ccf0d2f39f09beb7527121e719
XuhuiZhou <zhouxuhui2018@gmail.com>
2023-09-08 21:16:19 -0400

    ✨ add avatar

--- components/character.tsx
+++ components/character.tsx
@@ -20,8 +20,48 @@ export type Character = {
 }
 
 import Image, { StaticImageData } from 'next/image';
-import agent1 from "../assets/avatars/female/01H5TNE5P8F9NJ2QK2YP5HPXKH.svg";
-import agent2 from "../assets/avatars/male/01H5TNE5P83CZ1TDBVN74NGEEJ.svg";
+import agent1 from "../assets/avatars/male/avatar-svgrepo-com_1_blue.svg";
+import agent2 from "../assets/avatars/male/avatar-svgrepo-com_2_blue.svg";
+import agent3 from "../assets/avatars/male/avatar-svgrepo-com_3_blue.svg";
+import agent4 from "../assets/avatars/male/avatar-svgrepo-com_4_blue.svg";
+import agent5 from "../assets/avatars/male/avatar-svgrepo-com_5_blue.svg";
+import agent6 from "../assets/avatars/male/avatar-svgrepo-com_6_blue.svg";
+import agent7 from "../assets/avatars/male/avatar-svgrepo-com_7_blue.svg";
+import agent8 from "../assets/avatars/male/avatar-svgrepo-com_8_blue.svg";
+import agent9 from "../assets/avatars/male/avatar-svgrepo-com_1_green.svg";
+import agent10 from "../assets/avatars/male/avatar-svgrepo-com_2_green.svg";
+import agent11 from "../assets/avatars/male/avatar-svgrepo-com_3_green.svg";
+import agent12 from "../assets/avatars/male/avatar-svgrepo-com_4_green.svg";
+import agent13 from "../assets/avatars/male/avatar-svgrepo-com_5_green.svg";
+import agent14 from "../assets/avatars/male/avatar-svgrepo-com_6_green.svg";
+import agent15 from "../assets/avatars/male/avatar-svgrepo-com_7_green.svg";
+import agent16 from "../assets/avatars/male/avatar-svgrepo-com_8_green.svg";
+import agent17 from "../assets/avatars/male/avatar-svgrepo-com_1_purple.svg";
+import agent18 from "../assets/avatars/male/avatar-svgrepo-com_2_purple.svg";
+import agent19 from "../assets/avatars/male/avatar-svgrepo-com_3_purple.svg";
+import agent20 from "../assets/avatars/male/avatar-svgrepo-com_4_purple.svg";
+import agent21 from "../assets/avatars/female/avatar-svgrepo-com_1_blue.svg";
+import agent22 from "../assets/avatars/female/avatar-svgrepo-com_2_blue.svg";
+import agent23 from "../assets/avatars/female/avatar-svgrepo-com_3_blue.svg";
+import agent24 from "../assets/avatars/female/avatar-svgrepo-com_4_blue.svg";
+import agent25 from "../assets/avatars/female/avatar-svgrepo-com_5_blue.svg";
+import agent26 from "../assets/avatars/female/avatar-svgrepo-com_6_blue.svg";
+import agent27 from "../assets/avatars/female/avatar-svgrepo-com_7_blue.svg";
+import agent28 from "../assets/avatars/female/avatar-svgrepo-com_8_blue.svg";
+import agent29 from "../assets/avatars/female/avatar-svgrepo-com_9_blue.svg";
+import agent30 from "../assets/avatars/female/avatar-svgrepo-com_10_blue.svg";
+import agent31 from "../assets/avatars/female/avatar-svgrepo-com_1_green.svg";
+import agent32 from "../assets/avatars/female/avatar-svgrepo-com_2_green.svg";
+import agent33 from "../assets/avatars/female/avatar-svgrepo-com_3_green.svg";
+import agent34 from "../assets/avatars/female/avatar-svgrepo-com_4_green.svg";
+import agent35 from "../assets/avatars/female/avatar-svgrepo-com_5_green.svg";
+import agent36 from "../assets/avatars/female/avatar-svgrepo-com_6_green.svg";
+import agent37 from "../assets/avatars/female/avatar-svgrepo-com_7_green.svg";
+import agent38 from "../assets/avatars/female/avatar-svgrepo-com_8_green.svg";
+import agent39 from "../assets/avatars/female/avatar-svgrepo-com_9_green.svg";
+import agent40 from "../assets/avatars/female/avatar-svgrepo-com_10_green.svg";
+
+
 
 type Avatars = {
   [gender: string]: StaticImageData;
@@ -29,44 +69,45 @@ type Avatars = {
 
 const avatars: Avatars = {
   "Samuel Anderson": agent1,
-  "Zane Bennett": agent1,
-  "William Brown": agent1,
-  "Rafael Cortez": agent1,
-  "Noah Davis": agent1,
-  "Eli Dawson": agent1,
-  "Miles Hawkins": agent1,
-  "Hendrick Heinz": agent1,
-  "Benjamin Jackson": agent1,
-  "Ethan Johnson": agent1,
-  "Liam Johnson": agent1,
-  "Finnegan O'Malley": agent1,
-  "Jaxon Prentice": agent1,
-  "Donovan Reeves": agent1,
-  "Micah Stevens": agent1,
-  "Oliver Thompson": agent1,
-  "Ethan Smith": agent1,
-  "Oliver Smith": agent1,
-  "Baxter Sterling": agent1,
-  "Jasmine Blake": agent2,
-  "Sophia Brown": agent2,
-  "Mia Davis": agent2,
-  "Naomi Fletcher": agent2,
-  "Lena Goodwin": agent2,
-  "Lily Greenberg": agent2,
-  "Emily Harrison": agent2,
-  "Amara Hartley": agent2,
-  "Sophia James": agent2,
-  "Ava Martinez": agent2,
-  "Isabelle Martinez": agent2,
-  "Gwen Pierce": agent2,
-  "Sasha Ramirez": agent2,
-  "Giselle Rousseau": agent2,
-  "Mia Sanders": agent2,
-  "Calista Sinclair": agent2,
-  "Esmeralda Solis": agent2,
-  "Ava Thompson": agent2,
-  "Imelda Thorne": agent2,
-  "Isabella White": agent2,
+  "Zane Bennett": agent2,
+  "William Brown": agent3,
+  "Rafael Cortez": agent4,
+  "Noah Davis": agent5,
+  "Eli Dawson": agent6,
+  "Miles Hawkins": agent7,
+  "Hendrick Heinz": agent8,
+  "Benjamin Jackson": agent9,
+  "Ethan Johnson": agent10,
+  "Liam Johnson": agent11,
+  "Leo Williams": agent12,
+  "Finnegan O'Malley": agent20,
+  "Jaxon Prentice": agent13,
+  "Donovan Reeves": agent14,
+  "Micah Stevens": agent15,
+  "Oliver Thompson": agent16,
+  "Ethan Smith": agent17,
+  "Oliver Smith": agent18,
+  "Baxter Sterling": agent19,
+  "Jasmine Blake": agent40,
+  "Sophia Brown": agent21,
+  "Mia Davis": agent22,
+  "Naomi Fletcher": agent23,
+  "Lena Goodwin": agent24,
+  "Lily Greenberg": agent25,
+  "Emily Harrison": agent26,
+  "Amara Hartley": agent27,
+  "Sophia James": agent28,
+  "Ava Martinez": agent29,
+  "Isabelle Martinez": agent30,
+  "Gwen Pierce": agent31,
+  "Sasha Ramirez": agent32,
+  "Giselle Rousseau": agent33,
+  "Mia Sanders": agent34,
+  "Calista Sinclair": agent35,
+  "Esmeralda Solis": agent36,
+  "Ava Thompson": agent37,
+  "Imelda Thorne": agent38,
+  "Isabella White": agent39,
 };
 
 export const getAvatar = (agentInfo: string) => (
@@ -87,7 +128,7 @@ export const characterCard = (
               <div className="flex flex-row pt-2">
                 <div className="relative h-[50px] w-[50px] rounded-md border-[1px] border-gray-400 shadow-lg">
                   <div className="absolute inset-0 flex items-center justify-center">
-            {getAvatar(agent.gender)}
+            {getAvatar(agent.first_name+" "+agent.last_name)}
                   </div>
                 </div>
                 <div className="px-2">
