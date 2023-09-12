9a555cd3c771b33a32f18b53a2196f76e975d5bb
XuhuiZhou <zhouxuhui2018@gmail.com>
2023-09-12 13:25:41 -0400

    ✨ clean the chat

--- components/chat-panel.tsx
+++ components/chat-panel.tsx
@@ -77,29 +77,6 @@ export function ChatPanel({
         </Dialog><div className="fixed inset-x-0 bottom-0 bg-gradient-to-b from-muted/10 from-10% to-muted/30 to-50%">
                 <ButtonScrollToBottom />
                 <div className="mx-auto sm:max-w-2xl sm:px-4">
-                    <div className="flex h-10 items-center justify-center">
-                        {isLoading ? (
-                            <Button
-                                variant="outline"
-                                onClick={() => stop()}
-                                className="bg-background"
-                            >
-                                <IconStop className="mr-2" />
-                                Stop generating
-                            </Button>
-                        ) : (
-                            messages?.length > 0 && (
-                                <Button
-                                    variant="outline"
-                                    onClick={() => reload()}
-                                    className="bg-background"
-                                >
-                                    <IconRefresh className="mr-2" />
-                                    Regenerate response
-                                </Button>
-                            )
-                        )}
-                    </div>
                     <div className="space-y-4 border-t bg-background px-4 py-2 shadow-lg sm:rounded-t-xl sm:border md:py-4">
                         <ActionSelection setActionType={appendNoneOrLeaveDirectly} actionType={actionType} />
                         <PromptForm
--- components/prompt-form.tsx
+++ components/prompt-form.tsx
@@ -77,7 +77,7 @@ export function PromptForm({
                     rows={1}
                     value={input}
                     onChange={(e) => setInput(e.target.value)}
-                    placeholder="Send a message."
+                    placeholder="Input here."
                     spellCheck={false}
                     className="min-h-[60px] w-full resize-none bg-transparent px-4 py-[1.3rem] focus-within:outline-none sm:text-sm"
                 />
