0e6e03c4f415dfde12e760869211ff2ee69c0db2
Hao Zhu <prokilchu@gmail.com>
2023-09-11 23:07:59 -0400

    Add a block after chat list to make scroll work

--- components/chat-list.tsx
+++ components/chat-list.tsx
@@ -13,15 +13,15 @@ export function ChatList({ messages }: ChatList) {
     // }
 
     return (
-        <div className="relative mx-auto max-w-2xl px-4">
+        <><div className="relative mx-auto max-w-2xl px-4">
             {messages.map((message, index) => (
                 <div key={index}>
                     <ChatMessage message={message} />
                     {/* {index < messages.length - 1 && (
-            <Separator className="my-4 md:my-8" />
-          )} */}
+    <Separator className="my-4 md:my-8" />
+  )} */}
                 </div>
             ))}
-        </div>
+        </div><div className='h-[80px] w-full'> </div></>
     );
 }
