c356424ced68b8f337ce7a7e5a3afc49a7c0c524
sharonwx54 <sharon.wx.zhang@gmail.com>
2023-09-13 19:01:54 -0400

    adding scroll to selection bar and enable submit button to redirect

--- components/chat-feature-menu.tsx
+++ components/chat-feature-menu.tsx
@@ -19,11 +19,16 @@ import { IconExternalLink } from '@/components/ui/icons';
 export type ChatFeatureOptions = {
     scenarioList: string[];
     agentList: string[];
+    agentList2: string[];
 };
 
 export const chatFeatureOptionsExamples: ChatFeatureOptions = {
-    scenarioList: ['Scenario 1', 'Scenario 2', 'Scenario 3', 'Scenario 4'],
-    agentList: ['GPT-4', 'GPT-3.5', 'LLaMa-2'],
+    scenarioList: ['Scenario 1', 'Scenario 2', 'Scenario 3', 'Scenario 4', 
+    'Scenario 5', 'Scenario 6', 'Scenario 7', 'Scenario 8', 'Scenario 9'], 
+    // need to change to get all scenarios
+
+    agentList: ['GPT-4', 'GPT-3.5', 'LLaMa-2', 'Human'],
+    agentList2: ['GPT-4', 'GPT-3.5', 'LLaMa-2', 'Human'],
 };
 
 export interface ChatFeatureMenuProps {
@@ -34,23 +39,25 @@ interface FeatureDropdownListProps {
     buttonLabels: {
         scenarioButtonLabel: string;
         agentButtonLabel: string;
+        agentButtonLabel2: string;
     };
     chatFeatureOptions: {
         scenarioList: string[];
         agentList: string[];
+        agentList2: string[];
     };
     handleChoiceClick: (choice: string, labelType: string) => void;
 }
 
 const FeatureDropdownList: React.FC<FeatureDropdownListProps> = ({ buttonLabels, chatFeatureOptions, handleChoiceClick }) => {
     return (
-        <div className="flex justify-between space-x-8"> {/* Flex container for dropdowns */}
+        <div className="flex justify-between space-x-8 px-10"> {/* Flex container for dropdowns */}
             <div>
                 <label className="block text-sm font-medium leading-6">Select a scenario</label>
                 <div className="relative mt-2">
                     <DropdownMenu>
                         <DropdownMenuTrigger asChild>
-                            <Button className="relative flex h-[35px] w-[200px] items-center gap-x-1.5 rounded-md bg-background py-1.5 pl-3 pr-10 text-left text-sm font-semibold shadow-sm ring-2 ring-inset ring-gray-900 dark:ring-white text-gray-900 dark:text-white hover:bg-accent hover:text-accent-foreground">
+                            <Button className="relative flex h-[35px] w-[200px] items-center gap-x-1.5 rounded-md bg-background py-1.5 pl-3 pr-10 text-left text-sm font-semibold text-gray-900 shadow-sm ring-2 ring-inset ring-gray-900 hover:bg-accent hover:text-accent-foreground dark:text-white dark:ring-white">
                                 <div className="grow text-left">
                                     {buttonLabels.scenarioButtonLabel}
                                 </div>
@@ -66,21 +73,23 @@ const FeatureDropdownList: React.FC<FeatureDropdownListProps> = ({ buttonLabels,
                             align="start"
                             className="focus:outline-none] absolute z-10 mt-2 w-56 origin-top-right rounded-md shadow-lg ring-1 ring-black ring-opacity-5"
                         >
+                            <div className="max-h-40 overflow-y-auto"> 
                             {chatFeatureOptions.scenarioList.map((choice, index) => (
                                 <DropdownMenuItem key={index} className="flex-col items-start" onClick={() => handleChoiceClick(choice, 'scenarioButtonLabel')}>
                                     <div className="text-sm font-medium">{choice}</div>
                                 </DropdownMenuItem>
                             ))}
+                            </div>
                         </DropdownMenuContent>
                     </DropdownMenu>
                 </div>
             </div>
             <div>
-                <label className="block text-sm font-medium leading-6">Select an agent</label>
+                <label className="block text-sm font-medium leading-6">Select the 1st agent</label>
                 <div className="relative mt-2">
                     <DropdownMenu>
                         <DropdownMenuTrigger asChild>
-                            <Button className="relative flex h-[35px] w-[200px] items-center gap-x-1.5 rounded-md bg-background py-1.5 pl-3 pr-10 text-left text-sm font-semibold shadow-sm ring-2 ring-inset ring-gray-900 dark:ring-white text-gray-900 dark:text-white hover:bg-accent hover:text-accent-foreground">
+                            <Button className="relative flex h-[35px] w-[200px] items-center gap-x-1.5 rounded-md bg-background py-1.5 pl-3 pr-10 text-left text-sm font-semibold text-gray-900 shadow-sm ring-2 ring-inset ring-gray-900 hover:bg-accent hover:text-accent-foreground dark:text-white dark:ring-white">
                                 <div className="grow text-left">
                                     {buttonLabels.agentButtonLabel}
                                 </div>
@@ -105,6 +114,36 @@ const FeatureDropdownList: React.FC<FeatureDropdownListProps> = ({ buttonLabels,
                     </DropdownMenu>
                 </div>
             </div>
+            <div>
+                <label className="block text-sm font-medium leading-6">Select the 2nd agent</label>
+                <div className="relative mt-2">
+                    <DropdownMenu>
+                        <DropdownMenuTrigger asChild>
+                            <Button className="relative flex h-[35px] w-[200px] items-center gap-x-1.5 rounded-md bg-background py-1.5 pl-3 pr-10 text-left text-sm font-semibold text-gray-900 shadow-sm ring-2 ring-inset ring-gray-900 hover:bg-accent hover:text-accent-foreground dark:text-white dark:ring-white">
+                                <div className="grow text-left">
+                                    {buttonLabels.agentButtonLabel2}
+                                </div>
+                                <span className="pointer-events-none absolute inset-y-0 right-0 ml-3 flex items-center pr-2">
+                                    <svg className="-mr-1 h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                        <path fill-rule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clip-rule="evenodd" />
+                                    </svg>
+                                </span>
+                            </Button>
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent
+                            sideOffset={8}
+                            align="start"
+                            className="focus:outline-none] absolute z-10 mt-2 w-56 origin-top-right rounded-md shadow-lg ring-1 ring-black ring-opacity-5"
+                        >
+                            {chatFeatureOptions.agentList2.map((choice, index) => (
+                                <DropdownMenuItem key={index} className="flex-col items-start" onClick={() => handleChoiceClick(choice, 'scenarioButtonLabel')}>
+                                    <div className="text-sm font-medium">{choice}</div>
+                                </DropdownMenuItem>
+                            ))}
+                        </DropdownMenuContent>
+                    </DropdownMenu>
+                </div>
+            </div>
         </div>
     );
 };
@@ -116,9 +155,11 @@ export function ChatFeatureMenu({ chatFeatureOptions }: ChatFeatureMenuProps) {
     const [buttonLabels, setButtonLabels] = useState<{
         scenarioButtonLabel: string;
         agentButtonLabel: string;
+        agentButtonLabel2: string;
     }>({
         scenarioButtonLabel: chatFeatureOptionsExamples.scenarioList[0],
         agentButtonLabel: chatFeatureOptionsExamples.agentList[0],
+        agentButtonLabel2: chatFeatureOptionsExamples.agentList2[0],
     });
 
     const handleChoiceClick = (choice: string, optionType: string) => {
@@ -131,9 +172,13 @@ export function ChatFeatureMenu({ chatFeatureOptions }: ChatFeatureMenuProps) {
     const [isSubmitted, setIsSubmitted] = useState(false);
 
     const handleSubmitClick = () => {
-        console.log(`Selected scenario: ${buttonLabels.scenarioButtonLabel}. Selected agent: ${buttonLabels.agentButtonLabel}.`);
+        
+        const redirectURL = 'http://localhost:3000/render/01H99V82GR0V38B08K794D0JJ6';
+        console.log(`Selected scenario: ${buttonLabels.scenarioButtonLabel}. Selected 1st agent: ${buttonLabels.agentButtonLabel}.Selected 2nd agent: ${buttonLabels.agentButtonLabel2}.`);
         // TODO: submit
         setIsSubmitted(true);
+
+        window.location.href = redirectURL;
 
     };
 
@@ -144,7 +189,8 @@ export function ChatFeatureMenu({ chatFeatureOptions }: ChatFeatureMenuProps) {
                     {isSubmitted ? (
                         <div>
                             <p>Your selected scenario is {buttonLabels.scenarioButtonLabel}</p>
-                            <p>Your selected agent is {buttonLabels.agentButtonLabel}</p>
+                            <p>Your selected 1st agent is {buttonLabels.agentButtonLabel}</p>
+                            <p>Your selected 2nd agent is {buttonLabels.agentButtonLabel2}</p>
                         </div>
                     ) : (
                         <FeatureDropdownList
