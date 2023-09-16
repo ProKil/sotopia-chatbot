46345d96c2654272f6e63ec54b1e3f63298b7d49
Hao Zhu <prokilchu@gmail.com>
2023-09-16 15:49:46 -0400

    Action json encode bug (#26)
    * ✨ omit parameter in render page
    
    * 🐛 action json encode bug

--- components/chat-panel.tsx
+++ components/chat-panel.tsx
@@ -92,7 +92,10 @@ export function ChatPanel({
                             if(!id) throw new Error('id is not defined');
                             append({
                                 id,
-                                content: `{"action_type": "${ noneOrLeave}", "argument": ""}`,
+                                content: JSON.stringify({
+                                    action_type: noneOrLeave, 
+                                    argument: ''
+                                }),
                                 role: 'user',
                             });
                             setInput('');
@@ -132,7 +135,10 @@ export function ChatPanel({
                                 if (id) {
                                     await append({
                                         id,
-                                        content: `{"action_type": "${actionType}", "argument": "${value}"}`,
+                                        content: JSON.stringify({
+                                            action_type: actionType,
+                                            argument: value,
+                                        }),
                                         role: 'user',
                                     });
                                 }
