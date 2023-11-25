9c4bd1e00d7080069c188f0d25f4f9090179f1db
XuhuiZhou <zhouxuhui2018@gmail.com>
2023-11-25 16:33:59 -0500

    Update Sotopia server URL

--- app/render/[id]/page.tsx
+++ app/render/[id]/page.tsx
@@ -112,7 +112,7 @@ declare type GetEpisodeHelper = {
 };
 
 async function getEpisode(episodeId: string, omitModelNames: boolean): Promise<GetEpisodeHelper> {
-    const SOTOPIA_SERVER_URL = 'https://tiger.lti.cs.cmu.edu:8003/';
+    const SOTOPIA_SERVER_URL = 'http://54.227.18.156:8002/';
     if (SOTOPIA_SERVER_URL === undefined) {
         throw new Error('SOTOPIA_SERVER_URL is undefined');
     } else {
